7149121d3c38e458494fa036983cbfbd7195cda8
kaitokid252 <41147878+kaitokid252@users.noreply.github.com>
2021-10-05 20:19:54 +0700

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,87 @@
+CREATE DATABASE QuanLyNhaHang
+GO
+
+USE QuanLyNhaHang
+GO
+
+-- Food
+-- Table
+-- FoodCategory
+-- Account
+-- Bill
+-- BillInfo
+
+CREATE TABLE TableFood
+(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR(100)  NOT NULL,
+	status INT NOT NULL	-- 1 Co nguoi || 0 trong	
+)
+GO
+
+CREATE TABLE Account
+(
+	UserName NVARCHAR(100) PRIMARY KEY NOT NULL,	
+	PassWord NVARCHAR(1000) NOT NULL ,
+)
+GO
+CREATE TABLE Food
+(
+	id INT PRIMARY KEY,
+	name NVARCHAR(100) NOT NULL,
+	price INT NOT NULL,
+	count INT DEFAULT 0 NOT NULL,
+)
+GO
+CREATE TABLE Bill
+(
+	id INT IDENTITY PRIMARY KEY,
+	idTable INT NOT NULL,
+	CheckIn DATE NOT NULL,
+	Checkout DATE,
+	Totalprice INT NOT NULL DEFAULT 0,
+	status INT NOT NULL,
+	FOREIGN KEY (idTable) REFERENCES dbo.TableFood(id)
+)
+Go
+INSERT dbo.Food
+(
+    id,
+    name,
+    price,
+    count
+)
+VALUES
+(   2,   -- id - int
+    N'Gà', -- name - nvarchar(100)
+    300000,   -- price - int
+    1    -- count - int
+    )
+INSERT dbo.TableFood
+(
+    name,
+    status
+)
+VALUES
+(   N'', -- name - nvarchar(100)
+    0    -- status - int
+    )
+
+INSERT dbo.Account
+(
+    UserName,
+    PassWord
+)
+VALUES
+(   N'2', -- UserName - nvarchar(100)
+    N'1'  -- PassWord - nvarchar(1000)
+    )
+
+SELECT * FROM dbo.Account
+SELECT * FROM dbo.TableFood
+SELECT * FROM dbo.Food
+SELECT * FROM dbo.Bill
+SELECT * FROM dbo.BillInfo WHERE idBill
+DELETE FROM dbo.Bill
+
+
